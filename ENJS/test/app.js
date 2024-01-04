c12a8553291b01baa082ed686dcbdec7a42328cb
Chu Thị Yên <119092877+CHUTHIYE@users.noreply.github.com>
2024-01-04 20:02:03 +0700

    test

---
+++ ENJS/test/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const customerRouter = require("../routes/SongRoute");
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use("/api/song", customerRouter);
+
+app.listen(3000, () => {
+  console.log("server is runing on port 3000");
+});
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Yenct", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+module.exports = app;
